bbb1549c86e8b37ff6156a66f19f37f5f3203229
Jan Fluksa <janfluksa@outlook.com>
2025-03-19 22:07:34 +0100

    upload docs

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,14 @@
 <script setup>
 import Header from './components/Header.vue';
+import Section from './components/Section.vue';
 </script>
 
 <template>
   <Header />
+
+  <Section>
+    Ahoj
+  </Section>
 
   <main>
   </main>
---
+++ src/components/Section.vue
@@ -0,0 +1,12 @@
+<template>
+  <section>
+    <slot></slot>
+  </section>
+</template>
+
+<style scoped>
+section {
+  max-width: 1440px;
+  background-color: red;
+}
+</style>
