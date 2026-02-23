067dca79dfec307b4ace29aa1be6c23506e54d74
Jan Fluksa <janfluksa@outlook.com>
2026-02-23 12:40:53 +0100

    hmm

--- src/App.vue
+++ src/App.vue
@@ -11,17 +11,17 @@ import Tile from './components/Tile.vue';
   <Section>
     <Col>
 
-      <Tile image="/website-miki50.online/ondra.jpg">
+      <Tile image="website-miki50.online/ondra.jpg">
         <h2>Všechno nejlepší k narozeninám! </h2>
         <p>Ať jsi vždy minimálně tak šťastný jako já se 4 párky v rohlíku.</p>
       </Tile>
 
-      <Tile image="/website-miki50.online/lucka.jpeg">
+      <Tile image="website-miki50.online/lucka.jpeg">
         <h2>Nazdravi 🥂</h2>
         <p>...k tem 30. a 20. narozeninam.</p>
       </Tile>
 
-      <Tile image="/website-miki50.online/renca.jpg">
+      <Tile image="website-miki50.online/renca.jpg">
         <h2>Milý Miki,</h2>
         <p>K tvým kulatým narozeninám Ti přeju, aby každý nový den byl pro Tebe barevnější, 
           než kterýkoli obrázek, co vytvoříš. Ať si stále udržuješ ten skvělý humor a pohodu, 
@@ -32,40 +32,40 @@ import Tile from './components/Tile.vue';
       </Tile>
 
 
-      <Tile image="/website-miki50.online/martin.jpeg">
+      <Tile image="website-miki50.online/martin.jpeg">
         <h2>Všechno nejlepší Miki! </h2>
         <p>Ať máš pořád skvělé nápady, partu fajn lidí kolem sebe a udržíš si svou okouzlující upřímnost. Zdravíčko ať slouží a život ať tě baví.</p>
       </Tile>
 
-      <Tile image="/website-miki50.online/marek.jpg">
+      <Tile image="website-miki50.online/marek.jpg">
         <h2>Na zdraví a na dalších skvělých padesát! </h2>
         <p>Ať tě život stále baví, inspiruje a přináší spoustu úsměvů. Díky, že tu jsi s námi.</p>
       </Tile>
 
-      <Tile image="/website-miki50.online/petr.jpg">
+      <Tile image="website-miki50.online/petr.jpg">
         <h2>Všechno nejlepší </h2>
         <p>
           Hodně lásky nejen v práci!</p>
       </Tile>
 
-      <Tile image="/website-miki50.online/vitek.jpg">
+      <Tile image="website-miki50.online/vitek.jpg">
         <h2>Vše nejlepší, Miki.</h2>
         <p>Ať jsou tvé dny zalité sluncem a štestím, kamaráde.</p>
       </Tile>
 
-      <Tile image="/website-miki50.online/martind.jpg">
+      <Tile image="website-miki50.online/martind.jpg">
         <h2>Všechno nejlepší!</h2>
         <p>Díky za všechny naše společné diskuze! </p>
           <p>ps: Už jsi konečně hitnul ten gym? 🙂</p>
       </Tile>
 
 
-      <Tile image="/website-miki50.online/luky.jpg">
+      <Tile image="website-miki50.online/luky.jpg">
         <h2>Všechno nejlepší!</h2>
         <p>Hlavně pevné zdraví! Ať se ti za 50 let ještě staví 😀</p>
       </Tile>
 
-      <Tile image="/website-miki50.online/honza.jpg">
+      <Tile image="website-miki50.online/honza.jpg">
         <h2>Krásné narozeniny</h2>
         <p>Buď šťastný se svou rodinou a užívej si dobro.</p>
       </Tile>
@@ -74,24 +74,24 @@ import Tile from './components/Tile.vue';
 
     <Col>
 
-      <Tile video="/website-miki50.online/misa.mp4">
+      <Tile video="website-miki50.online/misa.mp4">
         <h2>Salud! 🥃</h2>
         <p>Ať ti to dál tančí jako doteď!</p>
       </Tile>
 
       
-      <Tile image="/website-miki50.online/alex.jpeg">
+      <Tile image="website-miki50.online/alex.jpeg">
         <h2>Všechno nejlepší</h2>
         <p>Buď turistou ve vlastním městě a vychutnej si dalších 50 let! ❤️</p>
       </Tile>
 
 
-      <Tile image="/website-miki50.online/michal.jpg">
+      <Tile image="website-miki50.online/michal.jpg">
         <h2>Všechno nejlepší</h2>
         <p>Ať je další 50 pořádná jízda, třeba i do Brna</p>
       </Tile>
 
-      <Tile image="/website-miki50.online/sarka.jpeg">
+      <Tile image="website-miki50.online/sarka.jpeg">
         <h2>Miki má narozeniny!!!</h2>
         <p>Přejeme ti se Žoldou všechno nejlepší! Všechno, co si přeješ, ať se vyplní 
           a ať jsi stále tak super.Doufám že spolu zažijeme ještě hodně srandy. </p>
@@ -99,7 +99,7 @@ import Tile from './components/Tile.vue';
       </Tile>
 
 
-      <Tile image="/website-miki50.online/kluci.jpg">
+      <Tile image="website-miki50.online/kluci.jpg">
         <h2>Hej MiX,</h2>
         <p>padesátka je jako software update <span style="display: inline-block;">– trochu</span> děsí, ale většinou přináší vylepšení! Přejeme ti méně bugů, žádné zpomalování a pořádnou dávku zábavy v každé nové verzi.</p>
         <p>
@@ -108,13 +108,13 @@ import Tile from './components/Tile.vue';
       </Tile>
 
       
-      <Tile image="/website-miki50.online/pavel.jpeg">
+      <Tile image="website-miki50.online/pavel.jpeg">
         <h2>Přeji vše nejlepší</h2>
         <p>...a hlavně to zdraví :-)</p>
       </Tile>
 
 
-      <Tile image="/website-miki50.online/lenka.jpg">
+      <Tile image="website-miki50.online/lenka.jpg">
         <h2>Happy B-Day</h2>
         <p>Nevadí, že jsi zase o rok starší, hlavní je, že jsi stále mladý duchem. ❤ Všechno nejlepší a připojuji malé překvápko 🙂
         </p>
@@ -122,7 +122,7 @@ import Tile from './components/Tile.vue';
       </Tile>
 
 
-      <Tile image="/website-miki50.online/verca.jpg">
+      <Tile image="website-miki50.online/verca.jpg">
         <h2>Vsechno je Miki ❤️</h2>
         <p>Jsi super chlap a já jsem moc ráda, že patříme do stejného týmu</p>
       </Tile>
